a43a26530d6e61f8a27d557fe988fd8f3e322a27
thinl3y <thinleedorg@gmail.com>
2026-09-22 13:28:52 +0600

    Initial Commit

---
+++ ZorigChusum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZorigChusum
+//
+//  Created by iMac03 on 9/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZorigChusum/ZorigChusumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZorigChusumApp.swift
+//  ZorigChusum
+//
+//  Created by iMac03 on 9/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ZorigChusumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
